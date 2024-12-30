f9bd18ab72dbc24406e6abd74c0687a591068e04
Lionel Mason <capitalcookhouse@gmail.com>
2024-12-30 07:40:06 -0500

    completes migrations-and-alembic

---
+++ lib/migrations/versions/222919d6675e_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 222919d6675e
+Revises: 
+Create Date: 2024-12-30 07:25:03.780933
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '222919d6675e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
